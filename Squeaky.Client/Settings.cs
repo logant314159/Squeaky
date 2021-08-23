b061b0523f3c10059162c6bb8ac38279184dc71c
Logan Thompson <logant314159@gmail.com>
2021-08-23 14:52:01 -0500

    .gitignore is now working.

---
+++ Squeaky.Client/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squeaky.Client
+{
+    public static class Settings
+    {
+        public static string SUBDIRECTORY = "";
+        public static string INSTALLNAME = "";
+        public static string INSTALLPATH = "";
+        public static string SERVER = "";
+        public static string TAG = "";
+        
+        public static bool INSTALL = false;
+        public static bool STARTUP = false;
+        public static bool LOGGER = false;
+
+        public static int SERVERPORT = 0;
+    }
+}
