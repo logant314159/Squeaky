baca8199f36f1b88ab492eabdc8530103fde5947
Logan Thompson <logant314159@gmail.com>
2021-08-23 14:58:00 -0500

    Cleaned up using statements in Settings.cs

--- Squeaky.Client/Settings.cs
+++ Squeaky.Client/Settings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Squeaky.Client
+﻿namespace Squeaky.Client
 {
     public static class Settings
     {
