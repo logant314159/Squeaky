53299f21b69551bf27a0cf52f018ef791990c2a5
Logan Thompson <logant314159@gmail.com>
2021-08-25 14:44:15 -0500

    Added debug settings configuration.

--- Squeaky.Client/Settings.cs
+++ Squeaky.Client/Settings.cs
@@ -4,6 +4,20 @@ namespace Squeaky.Client
 {
     public static class Settings
     {
+#if DEBUG
+        public static string SUBDIRECTORY = "client";
+        public static string INSTALLNAME = "squeakyClient";
+        public static string BASEPATH = @"C:\";
+        public static string INSTALLATION = Path.Combine(BASEPATH, (!string.IsNullOrEmpty(SUBDIRECTORY) ? SUBDIRECTORY + @"\" : "") + $@"{INSTALLNAME}\{INSTALLNAME}.exe");
+        public static string SERVER = "127.0.0.1";
+        public static string TAG = "debug";
+
+        public static bool INSTALL = false;
+        public static bool STARTUP = false;
+        public static bool LOGGER = false;
+
+        public static int SERVERPORT = 2000;
+#else
         public static string SUBDIRECTORY = "";
         public static string INSTALLNAME = "";
         public static string BASEPATH = "";
@@ -16,5 +30,6 @@ namespace Squeaky.Client
         public static bool LOGGER = false;
 
         public static int SERVERPORT = 0;
+#endif
     }
 }
